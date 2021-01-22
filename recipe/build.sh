6bc6791783da6410c03e3f099a5173dd2dca8e22
Johan Mabille <johan.mabille@gmail.com>
2021-01-22 15:19:14 +0100

    Release 0.0.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [[ ${target_platform} == linux-ppc64le ]]; then
-  cmake -DCMAKE_BUILD_TYPE=Release      \
-         -DBUILD_TESTS=OFF              \
-         -DCMAKE_INSTALL_PREFIX=$PREFIX \
-         -DCMAKE_INSTALL_LIBDIR=lib     \
-         -DXVG_DISABLE_TUNE_GENERIC=ON  \
+  cmake -DCMAKE_BUILD_TYPE=Release       \
+         -DBUILD_TESTS=OFF               \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX  \
+         -DCMAKE_INSTALL_LIBDIR=lib      \
+         -DXVEGA_DISABLE_TUNE_GENERIC=ON \
          $SRC_DIR
 else
-  cmake -DCMAKE_BUILD_TYPE=Release      \
-         -DBUILD_TESTS=OFF              \
-         -DCMAKE_INSTALL_PREFIX=$PREFIX \
-         -DCMAKE_INSTALL_LIBDIR=lib     \
-         -DXVG_DISABLE_ARCH_NATIVE=ON   \
+  cmake -DCMAKE_BUILD_TYPE=Release       \
+         -DBUILD_TESTS=OFF               \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX  \
+         -DCMAKE_INSTALL_LIBDIR=lib      \
+         -DXVEGA_DISABLE_ARCH_NATIVE=ON  \
          $SRC_DIR
 fi
 
