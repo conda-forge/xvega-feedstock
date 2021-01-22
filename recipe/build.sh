febda1892128356ca3bb5c2d7b936054286d8a0c
Johan Mabille <johan.mabille@gmail.com>
2021-01-22 12:28:31 +0100

    Fixed cmake command

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_LIBDIR=lib
+if [[ ${target_platform} == linux-ppc64le ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release      \
+         -DBUILD_TESTS=OFF              \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_INSTALL_LIBDIR=lib     \
+         -DXVG_DISABLE_TUNE_GENERIC=ON  \
+         $SRC_DIR
+else
+  cmake -DCMAKE_BUILD_TYPE=Release      \
+         -DBUILD_TESTS=OFF              \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_INSTALL_LIBDIR=lib     \
+         -DXVG_DISABLE_ARCH_NATIVE=ON   \
+         $SRC_DIR
+fi
+
 make -j ${CPU_COUNT} install
